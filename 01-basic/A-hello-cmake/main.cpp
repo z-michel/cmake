4f47625d155dfe4e7a20048ba8f4209bede84a6e
Zach Michel <zach.michel@smartthings.com>
2020-08-25 11:10:49 -0500

    GN Integration Progress

--- 01-basic/A-hello-cmake/main.cpp
+++ 01-basic/A-hello-cmake/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "hello_local.h"  			//Header in same directory as main.cpp
+#include "hello_shared.h" 			//GN Sample project header
+#include "hello_static.h"
+#include "goodbye.h"      	//Header in zach-dir/ subdirectory
 
 int main(int argc, char *argv[])
 {
    std::cout << "Hello CMake!" << std::endl;
+   printf("%s", GetStaticText());
+   print_hello();
+   print_goodbye();
    return 0;
 }
