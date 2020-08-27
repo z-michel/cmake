fe1589b3b70357491231156457d5e0e71399514a
Zach Michel <zach.michel@smartthings.com>
2020-08-27 12:32:47 -0500

    add chip

--- 01-basic/A-hello-cmake/main.cpp
+++ 01-basic/A-hello-cmake/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "lib/gn/examples/simple_build/hello.cc"
+
 int main(int argc, char *argv[])
 {
    std::cout << "Hello CMake!" << std::endl;
+   print_hello();
    return 0;
 }
